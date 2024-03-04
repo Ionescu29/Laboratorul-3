03077b38e0587db55323b5da44339a72efa1b783
Ionescu29 <160519352+Ionescu29@users.noreply.github.com>
2024-03-04 19:14:20 +0200

    Add files via upload

---
+++ Lab3Ex4/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//. Scrieti un program care va inversa elementele unui vector
+//Lungimea vectorului va fi citita de la tastatura
+Console.WriteLine("cate elemente doriti sa cititi");
+int n = int.Parse(Console.ReadLine());
+int[] vector = new int[n];
+//Inversarea elementelor va fi facuta in functia Main (fara o functie dedicate)
+//Afisarea vectorului se va face printr-o functie de afisare dedicata
+//nu imi dau seama cum ar trebui sa procedez ca sa schimb elementele unui vector;
