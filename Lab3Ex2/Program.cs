6a7c271f57c2cb20c30b9292672e674baadba602
Ionescu29 <160519352+Ionescu29@users.noreply.github.com>
2024-03-04 19:12:44 +0200

    Add files via upload

---
+++ Lab3Ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Scrieti o functie care va determina daca un numar este sau nu patrat perfect. Apelati-o si afisati-i rezultatul
+Console.WriteLine("introduceti numarul n");
+int n = int.Parse(Console.ReadLine());
+//urmeaza sa scriu functia
+int GetPatrat(int n)
+{
+    int patrat = 0;
+    for (var i=0;i<=n;i++)
+    {
+        patrat = patrat * n; 
+    }
+    return patrat;
+}
+Console.WriteLine(GetPatrat(n));
