94e48076c57f1b12cc45af2dfa812764cff4f607
Ionescu29 <160519352+Ionescu29@users.noreply.github.com>
2024-03-04 19:11:51 +0200

    Add files via upload

---
+++ Lab3Ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Scrieti o functie care va calcula suma cifrelor unui numar
+Console.WriteLine("introduceti numarul n");
+int n = int.Parse(Console.ReadLine());
+// AICI voi scrie functia 
+int GetSuma(int n)
+{
+    int suma = 0;
+    for(var i=0; i<=n;i++)
+    {
+        suma = n % 10 + n / 10;
+    }
+    return suma;
+}
